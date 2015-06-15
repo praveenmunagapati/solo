84d97771807aa1b25c237619c750ba878c7da96c
Tyler Travers <ttravers17@gmail.com>
2015-06-15 18:56:11 -0400

    smidge of code cleanup

--- static/main.js
+++ static/main.js
@@ -4,22 +4,20 @@ var $input = $('.query');
 $form.on('submit', function(e){
   e.preventDefault();
   askQuestion();
-  console.log($form.serialize());
-
-  function askQuestion() {
-    // Query paramater must be a string
-    $.ajax({
-      type: 'POST',
-      url: $SCRIPT_ROOT + 'chat',
-      data: $form.serialize(),
-      success: function(result){
-        var result = JSON.parse(result);
-        console.log(result.message)
-      },
-      fail: function(error) {
-        console.log(error);
-      }
-    });
-  };
-
+  $input.val('');
 });
+
+function askQuestion() {
+  $.ajax({
+    type: 'POST',
+    url: $SCRIPT_ROOT + 'chat',
+    data: $form.serialize(),
+    success: function(result){
+      var result = JSON.parse(result);
+      console.log(result.message)
+    },
+    error: function(error) {
+      alert(error);
+    }
+  });
+};
