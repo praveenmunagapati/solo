3a091cf9cc7c89a28ff489fbf23fcffcf5187235
Tyler Travers <ttravers17@gmail.com>
2015-06-15 18:04:19 -0400

    post requests going through, not correctly though

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, jsonify
 from flask import request
 from flask import render_template
 # from forms.QueryForm import CleverbotForm
@@ -6,19 +6,24 @@ from libs import cleverbot
 app = Flask(__name__, static_url_path='/static');
 
 cleverbot_client = cleverbot.Cleverbot()
+answers = []
 
 @app.route('/')
 def index():
   return render_template('index.html') 
 
+# @csrf.exempt
 @app.route('/chat', methods=["GET", "POST"])
 def cleverbot():
   if request.method == 'POST':
-    question = request.form['query']
+    question = request.args.get('question', type=str)
+    print(question)
     answer = cleverbot_client.ask(question);
-    return answer
+    answers.append(answer)
+    return jsonify(message=str(answer))
+    # return jsonify(message=str(answer))
   else:
-    return 'Not much here champ'
+    return jsonify(messages=str(answers))
 
 if __name__ == '__main__':
   app.run(debug=True)
--- static/main.js
+++ static/main.js
@@ -1,1 +1,25 @@
-console.log('ayy');
+$('.cleverbot').on('submit', function(e){
+  e.preventDefault();
+  var query = $(this).serialize().replace('query=', '');
+  // var q = $('.question').val();
+  // console.log(q);
+  // askQuestion(query);
+  console.log(query);
+  askQuestion(query);
+  function askQuestion(query) {
+    // Query paramater must be a string
+    $.ajax({
+      type: 'POST',
+      url: $SCRIPT_ROOT + 'chat',
+      // data: JSON.stringify({question: query}),
+      data: 'hello',
+      contentType: 'application/json;charset=UTF-8',
+      success: function(result){
+        console.log(result.message);
+      },
+      fail: function(error) {
+        console.log(error);
+      }
+    });
+  };
+});
