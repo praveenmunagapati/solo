d1aba92fa7200ede6590aaa61be16ba24d0d6572
Tyler Travers <ttravers17@gmail.com>
2015-06-15 16:58:16 -0400

    got cleverbot responding

--- app.py
+++ app.py
@@ -2,7 +2,10 @@ from flask import Flask
 from flask import request
 from flask import render_template
 # from forms.QueryForm import CleverbotForm
+from libs import cleverbot
 app = Flask(__name__, static_url_path='/static');
+
+cleverbot_client = cleverbot.Cleverbot()
 
 @app.route('/')
 def index():
@@ -11,7 +14,11 @@ def index():
 @app.route('/chat', methods=["GET", "POST"])
 def cleverbot():
   if request.method == 'POST':
-    return 'ayyyyyy'
+    question = request.form['query']
+    answer = cleverbot_client.ask(question);
+    return answer
+  else:
+    return 'Not much here champ'
 
 if __name__ == '__main__':
   app.run(debug=True)
